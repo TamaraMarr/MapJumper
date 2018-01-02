282e74e6194c1df9ca7e3927b6e78fadb0b7a0f0
Tamara Mardjetko <tasha.m.bg@gmail.com>
2018-01-02 23:19:02 +0100

    API key changed

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ export default class App extends Component {
 		return (
 			<div className="container">
 				<header className="App-header">
-					<h1>
+					<h1>API key 2
 						MapJumper
 					</h1>
 				</header>
 				<MapWithAMarker
 					center={{ lat: this.state.lat, lng: this.state.lng }}
-					googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+					googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCONmRM4vJlB7rYUMiaYb8tiIbyAWqRAsk&v=3.exp&libraries=geometry,drawing,places"
 					loadingElement={<div style={{ height: `100%` }} />}
 					containerElement={<div style={{ height: `400px` }} />}
 					mapElement={<div style={{ height: `100%` }} />}
