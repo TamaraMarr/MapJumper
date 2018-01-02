ab326f2cf4fb3bcf3d4dc17fdda5ea0c519c460d
Tamara Mardjetko <tasha.m.bg@gmail.com>
2018-01-02 23:35:40 +0100

    API key changed 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { MapWithAMarker } from "./Map";
 
 import "./App.css";
+import { API_KEY } from "./constants";
 
 export default class App extends Component {
 	constructor(props) {
@@ -30,7 +31,7 @@ export default class App extends Component {
 				</header>
 				<MapWithAMarker
 					center={{ lat: this.state.lat, lng: this.state.lng }}
-					googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCONmRM4vJlB7rYUMiaYb8tiIbyAWqRAsk&v=3.exp&libraries=geometry,drawing,places"
+					googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
 					loadingElement={<div style={{ height: `100%` }} />}
 					containerElement={<div style={{ height: `400px` }} />}
 					mapElement={<div style={{ height: `100%` }} />}
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const BASE_API_URL= "https://api.openweathermap.org/data/2.5/forecast?APPID=49aec73dd92d813a0b58b4129d67aecd&units=metric&q=";
+export const API_KEY="AIzaSyCONmRM4vJlB7rYUMiaYb8tiIbyAWqRAsk";
